c929badae32c6a868754b8ef4bb953d8eb615440
jkaddu <jkaddu@yahoo.com>
2019-05-23 15:49:44 -0700

    added create

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const db = require('./data/db.js');
 const server = express();
 
 server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+	const newUser = req.body;
+	console.log('request body: ', newUser);
+	db
+		.insert(newUser)
+		.then((users) => {
+			res.status(201).json(users);
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'Error saving user to database' });
+		});
+});
